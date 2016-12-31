2c10a01423c19f88ea9b960d2d723c1b1fa30e9f
huqc <huqc@gcks.cn>
2016-12-31 14:41:47 +0800

    First Commit

---
+++ FragmentBasics/app/src/main/java/com/kxylqc/fragmentbasics/ArticleFragment.java
@@ -0,0 +1,21 @@
+package com.kxylqc.fragmentbasics;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by pasenger on 2016/12/31.
+ */
+
+public class ArticleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        return null;
+    }
+}
